aaa5c7683f558d340d2135509dd2d5cc1277aa28
Marius Aleksander <marraleks@hotmail.com>
2019-09-13 14:00:32 +0200

    La til lik navigasjon mellom sidene

--- js/index.js
+++ js/index.js
@@ -1,7 +1,13 @@
 const overlay = document.querySelector(".overlay-menu");
 const knapp = document.querySelector("#knapp");
+const tilbake = document.querySelector("#tilbake");
+
+
 
 knapp.onclick = () => {
   overlay.classList.toggle("visible");
   knapp.classList.toggle("visible");
+  tilbake.classList.toggle("visible");
+
 }
+
